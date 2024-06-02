20dda55e28eac8f6e314a00e9988c9ca4e40ca71
Pronin Aleksey <black.pacman@outlook.com>
2024-06-02 13:56:46 +0300

    add some variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world1");
+﻿//Console.WriteLine("Hello world1");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
